13e951fc60d3b8cc7c1c5a05bb13ed071aa5e689
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-01-25 19:15:19 +0000

    adicionando versão do database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,18 @@
 import database from "infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+  });
 };
 
 export default status;
